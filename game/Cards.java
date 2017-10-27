5fcf84ca26be59f530fcc7a4f41cebd01be69904
Dmunoz <demuze28@gmail.com>
2017-10-27 10:38:59 -0400

    merge example 2

---
+++ game/Cards.java
@@ -0,0 +1,21 @@
+
+import java.util.Arrays;
+
+public class Card {
+
+    private int one = 1;
+    private int two = 2;
+    private int three = 3;
+    private int four = 4;
+    private int five = 5;
+    private int six = 6;
+    private int seven = 7;
+    private int eight = 8;
+    private int nine = 9;
+
+    private Arrays cards[108] = new ;
+
+
+
+
+}
